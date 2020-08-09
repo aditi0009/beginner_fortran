a3639f236ab5a99ba95d50af03f55c4102869c3b
aditi0009 <66020449+aditi0009@users.noreply.github.com>
2020-08-09 20:18:56 +0530

    Add files via upload

---
+++ fortranbegin.f95
@@ -0,0 +1,15 @@
+! 
+! 
+!                             Online Fortran Compiler.
+!                 Code, Compile, Run and Debug Fortran program online.
+! Write your code in this editor and press "Run" button to execute it.
+! 
+! 
+
+
+Program Hello
+implicit NONE
+print *, "Hello world"
+endProgram Hello
+
+
